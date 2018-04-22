f869eab5fd83245a5c9b60113007e4b4fc3c4241
Matias Szlajen <matias@Szlajen.M>
2018-04-22 19:51:53 -0300

    creacion de proyectos

---
+++ biblioCompartida/biblio.c
@@ -0,0 +1,9 @@
+/*
+ * biblio.c
+ *
+ *  Created on: Apr 22, 2018
+ *      Author: matias
+ */
+
+
+void prueba () {}
---
+++ biblioCompartida/biblio.h
@@ -0,0 +1,13 @@
+/*
+ * biblio.h
+ *
+ *  Created on: Apr 22, 2018
+ *      Author: matias
+ */
+
+#ifndef BIBLIO_H_
+#define BIBLIO_H_
+
+void prueba ();
+
+#endif /* BIBLIO_H_ */
