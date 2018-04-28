4763ccfe72657d6823866aae306e3a8f9b3dc992
arielpiwko <arielpiwko@gmail.com>
2018-04-28 01:40:25 -0300

    "Instancia: inicializar y conectar socket"

--- instancia/src/instancia.c
+++ instancia/src/instancia.c
@@ -10,8 +10,38 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+
+void inicializarSockaddr (struct sockaddr_in *direccion, int puerto){
+ int servidor;
+	direccion->sin_family = AF_INET;
+	direccion->sin_addr.s_addr = INADDR_ANY;
+	direccion->sin_port = htons(puerto);
+	servidor = socket(AF_INET, SOCK_STREAM, 0);
+	if (bind(servidor, (void*) &direccion, sizeof(direccion)) != 0){
+		perror("Falló el bind");
+		return;
+	}
+
+	printf("Estoy escuchando\n");
+	listen(servidor,100);
+ }
+
+void conectarSocket (int socketCoordinador,struct sockaddr_in direccion){
+	if( bind (socketCoordinador, (void*)&direccion , sizeof(struct sockaddr_in) ) != 0){
+			perror("No se pudo asociar el puerto, revisar bind\n");
+		}
+}
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+	struct sockaddr_in direccion;
+	int puerto = 8080;
+	int socketCoordinador = socket(AF_INET,SOCK_STREAM,0);
+
+	inicializarSockaddr(&direccion, puerto);
+	conectarSocket(socketCoordinador, direccion);
+
+		return EXIT_SUCCESS;
 }
