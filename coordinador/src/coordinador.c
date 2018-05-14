73020795acf6e88600f672f32eb7d019f546dcdc
Matias <mszlajen@est.frba.utn.edu.ar>
2018-05-14 20:09:12 -0300

    Comentado con correción

--- coordinador/src/coordinador.c
+++ coordinador/src/coordinador.c
@@ -12,7 +12,27 @@
 t_config * configuracion = NULL;
 t_list * listaInstancias = NULL;
 t_list * listaEsi = NULL;
+/*
+ * Comentario:
+ *
+ * Si igual lo van a hacer primero de todo
+ * (porque se hace en la inicialización),
+ * tambien pueden dar el valor inicial en la
+ * creación de la variable
+ *
+ * [MATI]
+ */
 int cantInstancias;
+/*
+ * Pregunta:
+ *
+ * ¿Esta variable para que la usan?
+ * Acuerdense que el ID de los ESI
+ * lo genera el Planificador si es
+ * para eso.
+ *
+ * [MATI]
+ */
 int cantEsi;
 
 int main(void) {
@@ -25,7 +45,24 @@ int main(void) {
 
 	socketCoordinador = crearSocketServer (IPEscucha,config_get_string_value(configuracion, Puerto));
 
-
+	/*
+	 * Si entiendo correctamente, este while se repite
+	 * hasta que se conecte el planificador, descartando
+	 * las conexiones que se puedan hacer antes.
+	 * Honestamente, no es la implementación que pense
+	 * para esto pero no está mal así que la pueden usar
+	 * si quieren pero con aclaraciones:
+	 * 1- Asegurar que se ejecute una iteración y despues
+	 * empezar a preguntar por la condición es exactamente
+	 * lo que hace do-while
+	 * 2-Por más que las conexiones anteriores no interesen
+	 * si llegamos a validar es que se creo un socket y por
+	 * lo tanto hay que cerrarlo en algún momento.
+	 * 2.2-Con 'en algún momento' me refiero a apenas saben
+	 * que no les sirve, en este caso.
+	 *
+	 * [MATI]
+	 */
 	while (esPlanificador) {
 		socketPlanificador = esperarYaceptar(socketCoordinador, 2 ,&dirPlanificador);
 		esPlanificador = validarPlanificador(socketPlanificador );
@@ -40,6 +77,17 @@ int main(void) {
 	}
 	//close(socketCoordinador);
 	exit(0);
+	/*
+	 * Comentario:
+	 * Ese exit(0) está al pedo porque no deberia llegar
+	 * en ejecución normal (ni en anormal por el while(1)
+	 * pero si llegara a ser utilizable tendrian que
+	 * reemplazarlo con una función que libere los recursos
+	 * y despues haga exit(0), como la de error pero sin el
+	 * error.
+	 *
+	 * [MATI]
+	 */
 }
 
 
@@ -47,11 +95,31 @@ void esESIoInstancia (int socketAceptado,struct sockaddr_in dir)
 {
 
 	int* mensaje;
+	/*
+	 * El warning ya les dice que le están pasando un int**
+	 * cuando espera un void** así que por lo menos les falta
+	 * el casteo. Ahora, si la idea es recibir el header
+	 * ¿por qué no usar el tipo header que está definido en
+	 * la biblioteca compartida?
+	 *
+	 * [MATI]
+	 */
 	int estadoDellegada = recibirMensaje(socketAceptado,4,&mensaje);
 
 	switch(mensaje[0]){
 
 	case 2:
+		/*
+		 * Les dice que están declarando de forma implicita
+		 * esta función (lo que es más o menos lo mismo que
+		 * no declararla) porque la usan acá pero está
+		 * definida más abajo de su implementación (o sea,
+		 * acá).
+		 * Pongan el prototipo en la biblioteca (junto con
+		 * todas las funciones que no lo tengan).
+		 *
+		 * [MATI]
+		 */
 		registrarInstancia(socketAceptado,mensaje);
 		break;
 
@@ -63,6 +131,15 @@ void esESIoInstancia (int socketAceptado,struct sockaddr_in dir)
 	default:
 
 		//Creo que no hace nada si no es Esi o Instancia
+		/*
+		 * No deberia pasar y no creo que lo prueben pero
+		 * por ser correctos, si al socket de recepción en
+		 * este punto le llega algo que no es ESI o Instancia
+		 * podes cerrar ese socket y mandar un mensaje de
+		 * error informando al usuario.
+		 *
+		 * [MATI]
+		 */
 		break;
 
 	}
@@ -76,6 +153,12 @@ void registrarEsi(int socket){
 
 	Esi nuevaEsi;
 	nuevaEsi.socket = socket;
+	/*
+	 * ¿Cuál es el punto de asignar el valor de una
+	 * variable con basura a otra que tambien tiene basura?
+	 *
+	 * [MATI]
+	 */
 	pthread_t hiloEsi;
 	nuevaEsi.hiloEsi = hiloEsi;
 
@@ -87,16 +170,49 @@ void registrarEsi(int socket){
 		{
 			cantEsi = list_add(listaEsi,&nuevaEsi);
 		}
+	/*
+	 * El cuarto parametro de pthread_create es void*
+	 * así que falta el casteo.
+	 *
+	 * [MATI]
+	 *
+	 * PD: Antes de que sigan este camino los invito
+	 * a que vean esto piensen si no hay otra manera
+	 * de resolverlo que crear un hilo por ESI.
+	 * https://github.com/sisoputnfrba/foro/issues/1012
+	 */
 	pthread_create(&nuevaEsi.hiloEsi, NULL, (void*) atenderEsi, socket);
 }
 
+/*
+ * Dice que hay un conflicto de tipos.
+ * No sabria el por qué exacto pero
+ * tiene que ver con las declaraciones
+ * implicitas así que arreglen eso y
+ * capaz se arregla.
+ *
+ * [MATI]
+ */
 void registrarInstancia(int socket,int* mensaje)
 {
 
 	Instancia instanciaRecibida;
 	instanciaRecibida.socket = socket;
+	/*
+	 * El warning en si les dice que le pasan
+	 * un puntero de puntero en lugar de un int
+	 * en el tercer parametro. Ahora:
+	 * Entiendo que intentan copiar el nombre
+	 * de la instancia en el struct que almacena
+	 * la información pero instanciaRecibida.nombre
+	 * es un puntero que va a apuntar a un espacio
+	 * de memoria (porque a este punto no lo hace
+	 * todavia). Revisen bien esto.
+	 *
+	 * [MATI]
+	 */
 	memcpy(&instanciaRecibida.nombre,(&mensaje) + 5,(&mensaje)+1);
-	instanciaRecibida.idinstancia =  cantInstancias;
+	instanciaRecibida.idinstancia = cantInstancias;
 
 	if (listaInstancias == NULL)
 		{
@@ -107,11 +223,28 @@ void registrarInstancia(int socket,int* mensaje)
 		{
 			cantInstancias = list_add(listaInstancias,&instanciaRecibida);
 		}
-
+	/*
+	 * Esto ya lo mencione en otro comentario pero
+	 * lo repito más claro:
+	 * 		Está mal usar un tipo de dato (por más
+	 * 		que sea el mismo) cuando hay otro definido
+	 * 		en la biblioteca compartida.
+	 *
+	 * [MATI]
+	 */
 	uint8_t protocolo = 5;
 	int copiar;
 	int * buffer = malloc(13);
 
+	/*
+	 * Relacionado al anterior.
+	 * Muchos tamaños hardcodeados.
+	 * ¿Qué pasa si en un momento nos damos
+	 * cuenta de un byte no alcanza y usamos
+	 * otro tipo de dato?
+	 *
+	 * [MATI]
+	 */
 	memcpy(buffer,&protocolo,1);
 
 	copiar = instanciaRecibida.idinstancia;
@@ -125,9 +258,26 @@ void registrarInstancia(int socket,int* mensaje)
 
 	int estado = enviarBuffer (instanciaRecibida.socket, buffer, 13);
 
+	/*
+	 * Tenemos en la commons una función hecha para imprimir errores,
+	 * si no la quieren usar alguna vez porque la alternativa es más
+	 * facil (como acá que hay que imprimir el nombre), está bien
+	 * pero usen el mismo formato.
+	 *
+	 * [MATI]
+	 */
 	if (estado != 0) {printf ("no se pudo enviar informacion de entradas a la instancia %s",instanciaRecibida.nombre ); }
 }
 
+/*
+ * Más de lo mismo:
+ * Es verdad que lo sockets son datos
+ * de tipo int pero para ayudar a evitar
+ * errores humanos tenemos definido el
+ * tipo socket_t
+ *
+ * [MATI]
+ */
 int esperarYaceptar(int socketCoordinador, int colaMax,struct sockaddr_in* dir)
 {
 	unsigned int tam;
@@ -140,16 +290,44 @@ int validarPlanificador (int socket) {
 	header* handshake = NULL;
 	int estadoDellegada;
 	uint8_t correcto = 1;
-
+	/*
+	 * Lo mismo que puse en el recibirMensaje anterior
+	 * sobre el tercer parametro.
+	 * En este caso el unico puede que hacer un casteo
+	 * de void** lo arregle y deje todo funcionando y
+	 * lindo.
+	 *
+	 * [MATI]
+	 */
 	estadoDellegada = recibirMensaje(socket,sizeof(header),&handshake);
-
-	if (estadoDellegada != 0){ printf ("no se pudo recivir header de planificador."); return 1; }
+	/*
+	 * Primer if:
+	 * 		1- El formato if(condicion)intrucciones funciona cuando
+	 * 		es algo chico (if(v > 0) v++; por ejemplo) sino complica
+	 * 		la lectura.
+	 * 		2- Vease lo que escribi más arriba de la función para
+	 * 		imprimir errores.
+	 * Segundo if:
+	 * 		No está "mal" pero si su idea era acortar espacio/comprimir
+	 * 		podian no usar las llaves o mucho mejor, sacar el if y hacer
+	 * 		return del operador ternario (?:)
+	 *
+	 * [MATI]
+	 */
+	if (estadoDellegada != 0){ printf ("no se pudo recibir header de planificador."); return 1; }
 	if (handshake->protocolo != correcto){ return 1; } else {return 0;}
 }
 
 
 void liberarRecursos()
 {
+	/*
+	 * Consejo:
+	 * 	Cada vez que agreguen un recurso, vengan
+	 * 	y ya dejan su liberación escrita.
+	 *
+	 * [MATI]
+	 */
 	if(configuracion != NULL)
 		config_destroy(configuracion);
 }
