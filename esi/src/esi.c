94a9672aaa23a003d161bf5bd0da742f998b03a8
arielpiwko <arielpiwko@gmail.com>
2018-06-07 02:43:53 -0300

    Progreso con ejecutar linea, sigo mañana

--- esi/src/esi.c
+++ esi/src/esi.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv) {
 
 	conectarConPlanificador();
 
-	esperarAvisoPlanificador(argv);
+	leerSiguienteInstruccion(argv);
 
 	puts("Salio todo bien\n");
 	liberarRecursos();
@@ -89,20 +89,28 @@ void liberarRecursos()
  * PD: Si sos de los que escriben todo y despues
  * lo declaran, ignora esto.
  */
-void esperarAvisoPlanificador(char** argv)
+void leerSiguienteInstruccion(char** argv)
 {
 	/*
 	 * Listen devuelve 1 o 0 para error y exito respectivamente,
 	 * no la cantidad de peticiones encoladas.
 	 * [MATI]
 	 */
-	int cantPeticiones = listen(socketPlan, 5);
 	char * linea;
+	char auxiliar;
+	char resultado;
+	int respuestaCoord;
 
-	while(cantPeticiones){
-		for(int i=cantPeticiones;i>0;i--){
+	if (listen(socketPlan, 5)){
 
 		programa = abrirArchivoLectura(argv[1]);
+
+		for(int i=0;i<5;i++){
+			while (linea != '/0' || feof(programa)){
+				linea = fread(auxiliar, sizeof("cadalinea"),1,programa);  //todo: size, usar commons
+				resultado = parse(linea);
+			}
+
 		/*
 		 * Lo que tenes que leer no tiene un tamaño fijo sino
 		 * que tenes que leer hasta encontrar el caracter de
@@ -116,28 +124,38 @@ void esperarAvisoPlanificador(char** argv)
 		 * PD: para solucionar esto va a tener que usar la
 		 * biblioteca de strings de las commons.
 		 */
-		linea = fread(argv, sizeof("cadalinea"),1,programa);
 
 		/*
 		 * Parse recibe una linea (char*) y devuelve el valor
 		 * de un t_esi_operacion (definido en el parser).
 		 * [MATI]
 		 */
-		parse(linea);
+
+
+			}
+			respuestaCoord = enviarInstruccionCoord(resultado);
+
+			if (respuestaCoord){
+				//bloqueo?
+			} else {
+				//avisar planificador
+			}
 
 		}
 	}
-}
 
-
-void enviarInstruccionCoord()
+int enviarInstruccionCoord(char instruccion)
 {
-	enviarProtocolo(socketCoord, 8);
+	int res = 0;
+	//enviarProtocolo(socketCoord, 8);
+	//res = ........ <--- todo: enviar instruccion del parametro al coordinador
+	return res;
 }
 
-int enviarProtocolo(socket_t sock, int protocolo)
+/*int enviarProtocolo(socket_t sock, int protocolo)
 {
 	header handshake;
 	handshake.protocolo = protocolo;
 	return enviarHeader(sock, handshake);
 }
+*/
