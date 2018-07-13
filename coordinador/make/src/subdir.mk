5da3f56e858eca21328794e035b7d86296622a48
Matias <mszlajen@est.frba.utn.edu.ar>
2018-07-13 17:30:15 -0300

    Automatización de setUp

--- coordinador/make/src/subdir.mk
+++ coordinador/make/src/subdir.mk
@@ -14,10 +14,10 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-src/coordinador.o: ../src/coordinador.c
+src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"../../biblioCompartida" -O0 -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/coordinador.d" -o "$@" "$<"
+	gcc -I"../../biblioCompartida" -O0 -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
