0788de8610b1632ade485ba60889ff4e839c2aac
Matias <mszlajen@est.frba.utn.edu.ar>
2018-07-17 16:04:13 -0300

    Clona los ESI si no están

--- pruebas/cloneESI.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/sisoputnfrba/Pruebas-ESI.git pruebas/esi
--- pruebas/setESI.sh
+++ pruebas/setESI.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[ ! -d pruebas/esi ] && git clone https://github.com/sisoputnfrba/Pruebas-ESI.git pruebas/esi || echo "Los ESI ya están clonados"
+read -n 1 -s
 make -C biblioCompartida/make
 read -n 1 -s
 make -C esi/make
